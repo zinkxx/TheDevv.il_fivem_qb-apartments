f99dc285ded6ed7187e4644f1fe8e621f15a05d3
Said <129689978+thedevvil@users.noreply.github.com>
2023-04-07 14:21:22 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,33 @@
+Apartments = {}
+
+Apartments.Starting = true
+
+Apartments.SpawnOffset = 30
+
+Apartments.Locations = {
+    ["apartment1"] = {
+        name = "apartment1",
+        label = "Motel",
+        coords = {
+            enter = vector4(435.73, 215.14, 103.17, 161.68),
+        }
+    },
+}
+
+Apartments.Language = {
+    ['enter'] = 'Odaya Gir',
+    ['ring_doorbell'] = 'Zili Çal',
+    ['logout'] = 'Çıkış Yap',
+    ['change_outfit'] = 'Kıyafet Değiştir',
+    ['open_stash'] = 'Depo',
+    ['move_here'] = 'Buraya Taşın',
+    ['open_door'] = 'Kapıyı Aç',
+    ['leave'] = 'Odadan Çık',
+    ['at_the_door'] = 'Kapıda birisi var!',
+    ['to_far_from_door'] = 'Kapı zilinden çok uzaktasın',
+    ['close_menu'] = "⬅ Menüyü Kapat",
+    ['tennants'] = 'Kiracılar',
+    ['nobody_home'] = 'Evde kimse yok',
+    ['receive_apart'] = "Bir dairen var",
+    ['changed_apart'] = "Daireyi taşıdın"
+}
